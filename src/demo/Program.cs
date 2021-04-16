6a30200329d3347535452be6974c0a5a87d5e48a
Kevin Huang <41318614+h2m730131@users.noreply.github.com>
2021-04-16 16:04:54 +0800

    清空 Main() 的 Code

--- src/demo/Program.cs
+++ src/demo/Program.cs
@@ -6,16 +6,6 @@ namespace solid_training
     {
         static void Main(string[] args)
         {
-            ShoppingCartContents shopCartContents = new()
-            {
-                items = new[] {new ShoppingCart {Price = 1, Quantity = 3},
-                               new ShoppingCart {Price = 2, Quantity = 2},
-                               new ShoppingCart {Price = 3, Quantity = 1}}
-            };
-
-            Order order = new(shopCartContents, 0.7f);
-            System.Console.WriteLine($"Order: {order.OrderTotal()}");
-
         }
     }
 }
